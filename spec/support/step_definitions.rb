0b2ec25b9a86bdcc8a66386a8f78ba519f456817
John Kelleher <johnpkelleher@gmail.com>
2013-04-30 13:24:54 -0400

    got user_publishes_post_spec to pass

--- spec/support/step_definitions.rb
+++ spec/support/step_definitions.rb
@@ -1,15 +1,15 @@
 def submit_form
-  find('input[type="submit"]').click
+  first('#sign_in_form').find('input[type="submit"]').click
 end
 
 def fill_in_sign_in_form(user = nil)
   user ||= FactoryGirl.create(:user)
-  fill_in 'user_email', with: user.email
-  fill_in 'user_password', with: user.password
+  first('#sign_in_form').fill_in 'user_email', with: user.email
+  first('#sign_in_form').fill_in 'user_password', with: user.password
 end
 
 def sign_in_as(user)
-  click_on t('visitors.sessions.new')
+  # click_on t('visitors.sessions.new')
   fill_in_sign_in_form(user)
   submit_form
 end
