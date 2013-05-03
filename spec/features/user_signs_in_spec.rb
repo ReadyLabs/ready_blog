73dd7cb47448463c5850f51855692d006d3dec89
John Kelleher <johnpkelleher@gmail.com>
2013-05-03 15:46:55 -0400

    don't show login form if signed in

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,9 +6,14 @@ feature 'Visitor signs in' do
     sign_in_as(FactoryGirl.create(:user))
 
     should_see_message_visitor_signed_in
+    should_not_see_sign_in_form
   end
 
   def should_see_message_visitor_signed_in
     page.should have_content(t('devise.sessions.signed_in'))
+  end
+
+  def should_not_see_sign_in_form
+    page.should_not have_css('#sign_in_form')
   end
 end
